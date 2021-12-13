997af020dcbed678a68c7d1b16dec83a1f86c631
Kevin C. Zimmerman <kevinczimmerman@gmail.com>
2021-12-13 11:20:35 -0600

    day2 part2

--- src/day2/main.rs
+++ src/day2/main.rs
@@ -8,24 +8,49 @@ fn main() -> io::Result<()> {
     let mut buf = String::new();
     f.read_to_string(&mut buf)?;
 
-    let mut submarine = Submarine::default();
-
+    let mut submarine = Submarine::<Part1>::default();
     submarine.run_instructions(buf.as_str());
-
     println!("part1: {}", submarine.depth * submarine.distance);
+
+    let mut submarine = Submarine::<Part2>::default();
+    submarine.run_instructions(buf.as_str());
+    println!("part2: {}", submarine.depth * submarine.distance);
 
     Ok(())
 }
 
 type Unit = i64;
 
-#[derive(Default)]
-struct Submarine {
-    distance: Unit,
-    depth: Unit,
+trait Operate {
+    fn operate(&mut self, c: Command);
 }
 
-impl Submarine {
+#[derive(Default)]
+struct Part1;
+#[derive(Default)]
+struct Part2;
+
+#[derive(Default)]
+struct Submarine<Part> {
+    distance: Unit,
+    depth: Unit,
+    aim: Unit,
+    phantom: std::marker::PhantomData<Part>,
+}
+
+impl<Part> Submarine<Part>
+where
+    Self: Operate,
+{
+    fn run_instructions(&mut self, commands: &str) {
+        commands
+            .lines()
+            .into_iter()
+            .for_each(|c| self.operate(Command::try_from(c).unwrap()));
+    }
+}
+
+impl Operate for Submarine<Part1> {
     fn operate(&mut self, c: Command) {
         match c {
             Command::Forward(n) => self.distance += n,
@@ -33,12 +58,18 @@ impl Submarine {
             Command::Up(n) => self.depth -= n,
         }
     }
+}
 
-    fn run_instructions(&mut self, commands: &str) {
-        commands
-            .lines()
-            .into_iter()
-            .for_each(|c| self.operate(Command::try_from(c).unwrap()));
+impl Operate for Submarine<Part2> {
+    fn operate(&mut self, c: Command) {
+        match c {
+            Command::Forward(n) => {
+                self.distance += n;
+                self.depth += self.aim * n
+            }
+            Command::Down(n) => self.aim += n,
+            Command::Up(n) => self.aim -= n,
+        }
     }
 }
 
@@ -66,20 +97,28 @@ impl TryFrom<&str> for Command {
 
 #[cfg(test)]
 mod tests {
-    use super::Submarine;
+    use super::{Part1, Part2, Submarine};
 
-    #[test]
-    fn test_submarine_commands() {
-        const COMMANDS: &'static str = "forward 5
+    const COMMANDS: &'static str = "forward 5
 down 5
 forward 8
 up 3
 down 8
 forward 2";
 
-        let mut submarine = Submarine::default();
+    #[test]
+    fn test_submarine_commands_part1() {
+        let mut submarine = Submarine::<Part1>::default();
         submarine.run_instructions(COMMANDS);
 
         assert_eq!(submarine.depth * submarine.distance, 150);
+    }
+
+    #[test]
+    fn test_submarine_commands_part2() {
+        let mut submarine = Submarine::<Part2>::default();
+        submarine.run_instructions(COMMANDS);
+
+        assert_eq!(submarine.depth * submarine.distance, 900);
     }
 }
