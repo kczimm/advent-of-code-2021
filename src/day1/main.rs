8cb45011a1afc7928f4fb2a940166a837a7d18e7
kczimm <kevinczimmerman@gmail.com>
2022-02-27 10:41:06 -0600

    add error handling to day1

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -4,6 +4,7 @@ use std::{
     fs::File,
     io::{self, Read},
 };
+use thiserror::Error;
 
 fn main() -> io::Result<()> {
     let mut f = File::open("src/day1/input.txt")?;
@@ -12,9 +13,9 @@ fn main() -> io::Result<()> {
 
     let report = Report::try_from(buf.as_str()).expect("parsing input failed");
 
-    let num_increases = report.num_increases(1);
+    let num_increases = report.num_increases(1).unwrap();
     println!("part1: {num_increases}");
-    let num_increases = report.num_increases(3);
+    let num_increases = report.num_increases(3).unwrap();
     println!("part2: {num_increases}");
 
     Ok(())
@@ -23,6 +24,12 @@ fn main() -> io::Result<()> {
 /// A sonar sweep measurement of [Depth].
 pub type Depth = i64;
 
+#[derive(Error, Debug, PartialEq)]
+pub enum ReportError {
+    #[error("window size must be positive")]
+    WindowSizeZero,
+}
+
 /// The sonar sweep [Report] contains measurements of the sea floor [Depth] as the sweep looks further and further away from the submarine.
 pub struct Report {
     measurements: Vec<Depth>,
@@ -30,11 +37,15 @@ pub struct Report {
 
 impl Report {
     /// Count the number of times a [Depth] measurement increases from the previous `window_size` measurements.
-    pub fn num_increases(&self, window_size: usize) -> usize {
-        assert!(window_size > 0, "window size must be positive");
+    ///
+    /// # Errors
+    /// Returns a ``WindowSizeZero`` when the `window_size` is 0.
+    pub fn num_increases(&self, window_size: usize) -> Result<usize, ReportError> {
+        if window_size == 0 {
+            return Err(ReportError::WindowSizeZero);
+        }
 
         let mut num = 0;
-
         if self.measurements.len() > window_size {
             for i in window_size..self.measurements.len() {
                 if (self.measurements[i] - self.measurements[i - window_size]) > 0 {
@@ -43,7 +54,7 @@ impl Report {
             }
         }
 
-        num
+        Ok(num)
     }
 }
 
@@ -64,7 +75,7 @@ impl TryFrom<&str> for Report {
 
 #[cfg(test)]
 mod tests {
-    use crate::Report;
+    use crate::{Report, ReportError};
 
     const INPUT: &str = "199
 200
@@ -81,7 +92,7 @@ mod tests {
     fn number_of_increases_window_size_1() {
         let report = Report::try_from(INPUT).unwrap();
         let window_size = 1;
-        assert_eq!(report.num_increases(window_size), 7);
+        assert_eq!(report.num_increases(window_size).unwrap(), 7);
     }
 
     #[test]
@@ -89,6 +100,17 @@ mod tests {
         let report = Report::try_from(INPUT).unwrap();
 
         let window_size = 3;
-        assert_eq!(report.num_increases(window_size), 5);
+        assert_eq!(report.num_increases(window_size).unwrap(), 5);
+    }
+
+    #[test]
+    fn bad_window_size() {
+        let report = Report::try_from(INPUT).unwrap();
+
+        let window_size = 0;
+        assert_eq!(
+            report.num_increases(window_size),
+            Err(ReportError::WindowSizeZero)
+        );
     }
 }
