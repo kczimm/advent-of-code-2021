b7bf36b94d5e98915fffbcedc1295f5ba47216fd
kczimm <kevinczimmerman@gmail.com>
2022-02-27 09:21:58 -0600

    apply new boilerplate

--- src/day6/main.rs
+++ src/day6/main.rs
@@ -1,5 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(clippy::all, clippy::pedantic)]
+
+use std::{
+    fs::File,
+    io::{self, Read},
+};
+
+fn main() -> io::Result<()> {
+    let mut f = File::open("src/day1/input.txt")?;
+    let mut buf = String::new();
+    f.read_to_string(&mut buf)?;
+
 }
 
 #[cfg(test)]
